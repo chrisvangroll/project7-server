14a31227b8d7484cbd6e8ac8635b0f009f8f5621
Chris VanGroll <chrisvangroll@gmail.com>
2021-10-06 11:58:16 -0500

    afdsfdas

--- app.js
+++ app.js
@@ -28,6 +28,15 @@ db.connect((err)=>{
 //middleware
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', '*')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    next()
+  })
+  
+
 app.use(helmet());
 
 app.use(bodyParser.json());
